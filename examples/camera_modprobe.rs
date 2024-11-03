de211307e0f3bab41940e7a460976686d175fe96
dvishal485 <26341736+dvishal485@users.noreply.github.com>
2024-11-03 20:23:19 +0530

    camera quick toggle button
    Signed-off-by: dvishal485 <26341736+dvishal485@users.noreply.github.com>

---
+++ examples/camera_modprobe.rs
@@ -0,0 +1,70 @@
+use anyhow::{Context, Error};
+use meh_asus::debugfs::common_hardware::camera_led::{CameraLedState, CAMERA_LED};
+use notify_rust::Notification;
+
+fn is_superuser() -> bool {
+    unsafe { libc::geteuid() == 0 }
+}
+
+const CAMERA_MODULE: &str = "uvcvideo";
+
+fn main() -> Result<(), Error> {
+    if !is_superuser() {
+        eprintln!("This program must be run as root");
+
+        Notification::new()
+            .appname("Asus Camera Module")
+            .summary("Camera module switch failed")
+            .body("This program must be run as root for the camera module to be enabled/disabled")
+            .show()
+            .context("Failed to show notification")?;
+
+        return Ok(());
+    }
+
+    let mut camera_led = CAMERA_LED;
+    let curr_state = camera_led.read()?;
+
+    match curr_state {
+        CameraLedState::On => {
+            let output = std::process::Command::new("modprobe")
+                .arg(CAMERA_MODULE)
+                .output()
+                .context("Failed to run modprobe")?;
+
+            if output.status.success() {
+                println!("Camera module {} has been enabled", CAMERA_MODULE,);
+                camera_led.apply(CameraLedState::Off)?;
+            } else {
+                eprintln!(
+                    "Failed to enable camera module {}:\n{}",
+                    CAMERA_MODULE,
+                    String::from_utf8_lossy(&output.stderr)
+                );
+                camera_led.apply(CameraLedState::Off)?;
+            }
+        }
+        CameraLedState::Off => {
+            let output = std::process::Command::new("rmmod")
+                .arg("-f")
+                .arg(CAMERA_MODULE)
+                .output()
+                .context("Failed to run rmmod")?;
+
+            if output.status.success() {
+                println!("Camera module {} has been disabled", CAMERA_MODULE);
+                camera_led.apply(CameraLedState::On)?;
+            } else {
+                eprintln!(
+                    "Failed to disable camera module {}:\n{}",
+                    CAMERA_MODULE,
+                    String::from_utf8_lossy(&output.stderr),
+                );
+                camera_led.apply(CameraLedState::Off)?;
+            }
+        }
+    }
+
+    println!("Camera LED status: {:?}", camera_led.read_ref());
+    Ok(())
+}
