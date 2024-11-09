d51f53f78d8633977b794c3cce8552d8b10adad5
dvishal485 <26341736+dvishal485@users.noreply.github.com>
2024-11-09 14:11:46 +0530

    name space separation when pwm is enabled
    Signed-off-by: dvishal485 <26341736+dvishal485@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-mod debugfs;
+#[cfg(feature = "pwm")]
+pub mod debugfs;
 #[cfg(feature = "pwm")]
 pub mod pwm;
+
+#[cfg(not(feature = "pwm"))]
+mod debugfs;
+#[cfg(not(feature = "pwm"))]
 pub use debugfs::*;
