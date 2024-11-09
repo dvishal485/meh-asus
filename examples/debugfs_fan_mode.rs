354c1b31eda9a204e3fef2c8b1182569a9ca38a5
dvishal485 <26341736+dvishal485@users.noreply.github.com>
2024-11-09 12:29:57 +0530

    add fan mode control like windows
    Signed-off-by: dvishal485 <26341736+dvishal485@users.noreply.github.com>

---
+++ examples/debugfs_fan_mode.rs
@@ -0,0 +1,18 @@
+use meh_asus::debugfs::common_hardware::fan::{FanMode, FAN};
+use meh_asus::debugfs::error::HardwareError;
+
+fn main() -> Result<(), HardwareError> {
+    let next_fan_mode = match FAN.read()? {
+        FanMode::Whispher => FanMode::Standard,
+        FanMode::Standard => FanMode::Performace,
+        FanMode::Performace => FanMode::FullSpeed,
+        FanMode::FullSpeed => FanMode::Whispher,
+    };
+
+    FAN.apply(next_fan_mode)?;
+
+    assert_eq!(FAN.read()?, next_fan_mode, "Failed to switch fan mode");
+    println!("Switched fan mode to {:?}", next_fan_mode);
+
+    Ok(())
+}
