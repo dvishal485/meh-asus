f63f461e995af10a7ed80d0045c8ffe025c5e5d0
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2024-12-01 15:08:42 +0530

    update fan mode chaneg script
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- examples/debugfs_fan_mode.rs
+++ examples/debugfs_fan_mode.rs
@@ -1,20 +1,30 @@
-use anyhow::Error;
+use std::process::ExitCode;
+
 use fan::FanMode;
 use meh_asus::common_hardware::fan;
 
-fn main() -> Result<(), Error> {
+fn main() -> ExitCode {
     let fan = fan::get();
-    let next_fan_mode = match fan.read()? {
-        FanMode::Whispher => FanMode::Standard,
-        FanMode::Standard => FanMode::Performace,
-        FanMode::Performace => FanMode::FullSpeed,
-        FanMode::FullSpeed => FanMode::Whispher,
+    let next_fan_mode = match fan.read() {
+        Ok(curr_fan_mode) => match curr_fan_mode {
+            FanMode::Whispher => FanMode::Standard,
+            FanMode::Standard => FanMode::Performace,
+            FanMode::Performace => FanMode::FullSpeed,
+            FanMode::FullSpeed => FanMode::Whispher,
+        },
+        Err(_) => FanMode::Standard,
     };
 
-    fan.apply(next_fan_mode)?;
-
-    assert_eq!(fan.read()?, next_fan_mode, "Failed to switch fan mode");
-    println!("Switched fan mode to {:?}", next_fan_mode);
-
-    Ok(())
+    if let Err(e) = fan.apply(next_fan_mode) {
+        eprintln!("Failed to apply fan mode!\n{:?}", e);
+        ExitCode::FAILURE
+    } else {
+        if let Ok(true) = fan.read().map(|curr_mode| curr_mode == next_fan_mode) {
+            println!("Switched fan mode to {:?}", next_fan_mode);
+            ExitCode::SUCCESS
+        } else {
+            eprintln!("Failed to switch fan mode!");
+            ExitCode::FAILURE
+        }
+    }
 }
